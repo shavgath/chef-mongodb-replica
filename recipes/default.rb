afe981aa86ac99746214f79a4877ca255130623c
Shavgath Perumal <48409809+shavgath@users.noreply.github.com>
2019-09-02 09:46:57 +0100

    Updated Cookbook

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,5 @@
 #
-# Cookbook:: mongodb
+# Cookbook:: mongo
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
@@ -8,12 +8,12 @@ apt_update 'update_sources' do
 end
 
 apt_repository 'mongodb-org' do
- uri "http://repo.mongodb.org/apt/ubuntu"
- distribution 'trusty/mongodb-org/3.2'
- components ['multiverse']
- keyserver 'hkp://keyserver.ubuntu.com:80'
- key 'EA312927'
- action :add
+  uri 'http://repo.mongodb.org/apt/ubuntu'
+  distribution 'trusty/mongodb-org/3.2'
+  components ['multiverse']
+  keyserver 'hkp://keyserver.ubuntu.com:80'
+  key 'EA312927'
+  action :add
 end
 
 directory '/data/' do
@@ -39,7 +39,7 @@ end
 
 template '/etc/systemd/system/mongod.service' do
   source 'mongod.service.erb'
-  mode '0755'
+  mode '0600'
   owner 'root'
   group 'root'
 end
